dbdabf576d044bd65addcc4b419bc176b972d378
CristianBudala <cristian.budala1@gmail.com>
2026-08-30 19:56:16 +0300

    two more instructions

--- main_6502.cpp
+++ main_6502.cpp
@@ -6,63 +6,126 @@ using Word = unsigned short;
 
 using u32 = unsigned int;
 
-struct Mem
-{
+struct Mem {
     static constexpr u32 MAX_MEM = 1024 * 64;
     Byte Data[MAX_MEM];
 
-    void Initialise(){
-        for(u32 i{}; i < MAX_MEM; i++ )    Data[i] = 0;
+    void Initialise() {
+        for (u32 i{}; i < MAX_MEM; i++)
+            Data[i] = 0;
     }
 
-    Byte operator[](u32 Address) const{
+    // Read 1 byte
+    Byte operator[](u32 Address) const {
+        return Data[Address];
+    }
+
+    // Write 1 byte
+    Byte& operator[](u32 Address) {
         return Data[Address];
     }
 };
 
-struct CPU
-{
-    Word PC; // Program counter
-    Word SP; // Stack pointer
-    Byte A, X, Y; // Registers: Accumulator, X, Y
-    
-    // Status flags
-    Byte C : 1; // Carry
-    Byte Z : 1; // Zero
-    Byte I : 1; // Interrupt
-    Byte D : 1; // Decimal
-    Byte B : 1; // Break
-    Byte V : 1; // Overflow flag
-    Byte N : 1; // Negative flag
+struct CPU {
+    Word PC;       // Program counter
+    Word SP;       // Stack pointer
+    Byte A, X, Y;  // Registers: Accumulator, X, Y
 
-    void Reset(Mem& memory){
+    // Status flags
+    Byte C : 1;  // Carry
+    Byte Z : 1;  // Zero
+    Byte I : 1;  // Interrupt
+    Byte D : 1;  // Decimal
+    Byte B : 1;  // Break
+    Byte V : 1;  // Overflow flag
+    Byte N : 1;  // Negative flag
+
+    void Reset(Mem& memory) {
         PC = 0XFFFC;
         SP = 0X0100;
         D = 0;
-        C = Z= I = D = B = V = N = 0;
+        C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
         memory.Initialise();
     }
 
-    Byte FetchByte(u32& Cycles, Mem& memory){
+    Byte FetchByte(u32& Cycles, Mem& memory) {
         Byte data = memory[PC];
         PC++;
         Cycles--;
         return data;
     }
 
-    void Execute(u32 Cycles, Mem& memory){
-        while(Cycles) {
-            Byte Instruction = FetchByte(Cycles, memory);
+    Word FetchWord(u32& Cycles, Mem& memory) {
+        // 6502 is little endian
+        Word data = memory[PC];
+        PC++;
+        
+        data |= (memory[PC] << 8);
+        PC++;
+
+        Cycles -= 2;
+        return data;
+    }
+
+    Byte ReadByte(u32& Cycles, Byte Address, Mem& memory) {
+        Byte data = memory[Address];
+        Cycles--;
+        return data;
+    }
+
+    // OPCODES
+    static constexpr Byte 
+        INS_LDA_IMM = 0xA9,
+        INS_LDA_ZP = 0xA5,
+        INS_LDA_ZPX = 0xB5;
+
+    void LDASetStatus(){
+        Z = (A == 0);
+        N = (A & 0x1000000) > 0;
+    }
+
+    void Execute(u32 Cycles, Mem& memory) {
+        while (Cycles) {
+            Byte Ins = FetchByte(Cycles, memory);
+
+            switch (Ins) {
+                case INS_LDA_IMM: {
+                    Byte Value = FetchByte(Cycles, memory);
+                    A = Value;
+                    LDASetStatus();
+                } break;
+
+                case INS_LDA_ZP: {
+                    Byte ZeroPageAddr = FetchByte(Cycles, memory);
+                    A = ReadByte(Cycles, ZeroPageAddr, memory);
+                    LDASetStatus();
+                } break;
+
+                case INS_LDA_ZPX: {
+                    Byte ZeroPageAddr = FetchByte(Cycles, memory);
+                    ZeroPageAddr += X;
+                    Cycles--;
+                    A = ReadByte(Cycles, ZeroPageAddr, memory);
+                    LDASetStatus();
+                } break;
+
+                default: {
+                    printf("Instruction not handled %d", Ins);
+                } break;
+            }
         }
     }
 };
 
-int main()
-{
+int main() {
     Mem mem;
     CPU cpu;
     cpu.Reset(mem);
+
+    mem[0xFFFC] = CPU::INS_LDA_IMM;
+    mem[0xFFD] = 0x42;
+
     cpu.Execute(2, mem);
     return 0;
 }
